b7ce4892bff9099bb2aa9369e70e405ac626230e
Luke Salazar <lucassy07@outlook.com>
2023-06-29 15:41:40 -0500

    About mid

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -15,9 +15,21 @@ const About = () => {
           <AnimatedLetters 
             letterClass={letterClass}
             strArr={abtArr}
-            idx={15}
+            idx={10}
           />
         </h1>
+        <p>
+          Passionate and curious web developer who thrives on the ever-evolving nature of technology. With a deep fascination for the digital world, I embark on a constant quest for knowledge, always seeking to expand my skills and stay ahead of the curve. 
+        </p>
+        <p>
+          At the core of my development philosophy is the belief that learning is a lifelong process. As a dedicated and self-motivated learner, I embrace every opportunity to enhance my expertise and push the boundaries of what is possible in web development.
+        </p>
+        <p>
+          I am deeply passionate about web development, driven by my desire to create innovative and impactful digital solutions. Whether it's architecting efficient backend systems or crafting seamless frontend and backend interactions, I am excited to take on diverse challenges and collaborate with experienced developers to further refine my abilities. With a growth mindset and a thirst for knowledge, I am committed to continuously learning and developing my skills as a versatile web developer. 
+        </p>
+        <p>
+          Join me as I share my experiences and creations, fueled by my insatiable curiosity.
+        </p>
       </div>
     </div>
   )

6b4f88efa5c7f383dfe6b4fdcadc4de29f00af94
Luke Salazar <lucassy07@outlook.com>
2023-07-10 16:23:32 -0500

    first setting of EmailJS

--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -1,7 +1,8 @@
 import Loader from 'react-loaders';
 import './contact.scss';
 import AnimatedLetters from '../AnimatedLetters/animatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
@@ -9,11 +10,27 @@ const Contact = () => {
   const contact: string = "Contact me";
   const contactArr: string[] = contact.split(''); 
 
+  const refForm = useRef;
+
   useEffect(() => {
     setTimeout(() => {
       return setLetterClass('text-animate-hover')
     }, 3000)
   }, []);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service',
+      'template',
+      refForm.current,
+      'key'
+
+    )
+    
+  }
+
 
   return (
     <>
@@ -26,7 +43,7 @@ const Contact = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dicta aperiam recusandae amet vitae odio quas a itaque alias iste expedita, quidem officiis debitis enim laboriosam quibusdam, excepturi quis facere!
           </p>
           <div className="contact-form">
-            <form action="">
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <div className="basic-data">
                   <li className="half">

7a6dc40424e0de7a867198356ed06962eb348f2a
Luke <lucassy07@outlook.com>
2023-07-11 22:20:18 -0500

    path to Cvs

--- src/components/CV/cv.tsx
+++ src/components/CV/cv.tsx
@@ -31,13 +31,13 @@ const Cv = () => {
           <h2>Here you can get my CV in your preferred language</h2>
           <div className="cvs-container">
             <div className="each-cv-cont">
-              <a className='to-CV' href='/CV'>
+              <a className='to-CV' href={process.env.PUBLIC_URL + '/assets/docs/CV_Lucas_Salazar.pdf'}>
                 <FontAwesomeIcon icon={faFilePdf} className='CV-icon' />
                 <h3>English</h3>
               </a>
             </div>
             <div className="each-cv-cont">
-              <a className='to-CV' href='/CV'>
+              <a className='to-CV' href={process.env.PUBLIC_URL + '/assets/docs/CV_Lucas_Salazar_ESP.pdf'}>
                 <FontAwesomeIcon icon={faFilePdf} className='CV-icon' />
                 <h3>Spanish</h3>
               </a>

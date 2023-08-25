37e384674c0560a055325cea30b4a051e166a3af
Luke Salazar <lucassy07@outlook.com>
2023-08-25 10:34:08 -0500

    Feat: Projects section

--- src/components/Projects/projects.tsx
+++ src/components/Projects/projects.tsx
@@ -45,7 +45,7 @@ const Projects: React.FC = () => {
             Here is a concise compilation of key projects I have contributed to as a web developer. Each project includes descriptions, the tech stack utilized, <span>the source code repository</span>, my main role and functions, insights, obstacles, learnings, work process, comments, and more.
           </p>
           <p id='note'>
-            <span>***</span>Please note that this section of the portfolio is a work in progress, and certain projects may not be deployed due to project scope.
+            <span>***</span>Please note that this section of the portfolio may contain certain projects not yet deployed due its scope. However, the access to each <i>GitHub repository</i> and source code is available.
           </p>
         </div>
         <div className="projects__area">
@@ -64,11 +64,11 @@ const Projects: React.FC = () => {
             {selectedProject && (
               <Modal onClose={handleCloseModal}>
                 <h3>{selectedProject.name}</h3>
-                <img src={selectedProject.img} alt={`Imag related to the ${selectedProject.name} project`} />
+                <img src={selectedProject.img} alt={`Logo related to the ${selectedProject.name} project`} />
                 <p>{selectedProject.description}</p>
-                <p>Tech Stack: {selectedProject.stack.join(" | ")}</p>
-                <p>Role: {selectedProject.role}</p>
-                <p>{selectedProject.repo}</p>
+                <p><strong>Tech Stack:</strong> {selectedProject.stack.join(" | ")}</p>
+                <p><strong>Role:</strong> {selectedProject.role}</p>
+                <a href={selectedProject.repo} target='_blank' rel='noreferrer'><p>Project's Source Code</p></a>
               </Modal>
             )}
 
